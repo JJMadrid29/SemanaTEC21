96cdc39a6b159d8af19ae6a69d73e1d9149fa8ac
JorgeEstebanCardenas <83671516+JorgeEstebanCardenas@users.noreply.github.com>
2021-05-06 15:46:53 -0700

    Añaden comentarios

--- filtro.py
+++ filtro.py
@@ -73,12 +73,16 @@ for (x, y, w, h) in faces:
         bg_black = bg_black[dif:, :, 0:3]
         bg_frame = cv2.bitwise_and(n_frame, n_frame, mask=mask_inv[dif:,:])
         
+        # Sumamos las dos imágenes obtenidas
         result = cv2.add(bg_black, bg_frame)
         if yi >= 0:
             frame[yi : yf, x : x + col_image] = result
 
         else:
             frame[0 : yf, x : x + col_image] = result
+        
+        
+         
        
      
       
