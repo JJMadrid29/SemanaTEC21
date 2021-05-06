9fcc62588f3b444dfa1221e2a9be23b16d07c3ff
SiddLopez <83721976+SiddLopez@users.noreply.github.com>
2021-05-06 13:26:06 -0700

    Añadir comentarios

--- filtro.py
+++ filtro.py
@@ -1,6 +1,8 @@
+# Importar librerías
 import cv2
 import imutils
 
+# Capturar video, cambiar parametros segun nuestra camara
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
 image = cv2.imread('sidd.png', cv2.IMREAD_UNCHANGED)
